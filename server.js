22c15c27419ad3923db0f8ee6b56a711f88b092a
Ilham Maulana <abdasykal@gmail.com>
2026-09-25 16:05:34 +0700

    perbaiki port server untuk railway

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
+
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
+// CONTOH BENAR (Dinamis untuk Railway):
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server berjalan di port ${PORT}`);
 });
 
 // Konfigurasi Supabase (Ganti dengan URL dan Anon Key project Supabase kamu)
